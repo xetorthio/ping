dc1bc3b34963543421fc90383a80240fc9f2144c
Jonathan Leibiusky <jleibiusky@gilt.com>
2014-07-17 11:28:57 -0400

    Add ionblaster metdata to be able to deploy

--- Dockerfile
+++ Dockerfile
@@ -1,8 +1,4 @@
-FROM ubuntu:13.04
-RUN apt-get install -y software-properties-common
-RUN add-apt-repository ppa:chris-lea/node.js
-RUN apt-get update
-RUN apt-get install -y nodejs
+FROM google/nodejs
 ADD app.js /app/app.js
 WORKDIR /app
 CMD node app.js
--- app.js
+++ app.js
@@ -4,6 +4,16 @@ http.createServer(function(req, res) {
   if(req.url == '/ping') {
     res.writeHead(200);
     res.end('pong');
+  } else if(req.url == '/echo') {
+    res.writeHead(200);
+    var body = '';
+    req.on('data', function(d) {
+      body += d;
+    });
+    req.on('end', function() {
+      var lala = JSON.parse(body);
+      res.end(lala.Reason);
+    });
   } else {
     res.writeHead(404);
     res.end();
